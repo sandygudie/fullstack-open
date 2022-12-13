a19042ee4c932dfb3fbf41a4a83bbcf4abcca95e
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2022-12-13 16:50:23 +0100

    commit

--- backend/models/person.js
+++ backend/models/person.js
@@ -1,16 +1,5 @@
-require("dotenv").config();
+
 const mongoose = require("mongoose");
-
-const url = process.env.MONGODB_URI;
-
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
 
 const personSchema = new mongoose.Schema({
   name: {
